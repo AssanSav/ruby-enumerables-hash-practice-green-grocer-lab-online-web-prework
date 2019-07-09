49ca4007ce8f5f534842a9b012c39eda9a2e03e7
assane savadogo <assane.savadogo81@gmail.com>
2019-07-09 23:55:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -45,7 +45,7 @@ end
 
 def checkout(cart, coupons)
   cart = consolidate_cart(cart)
-  cart = apply_coupons(coupons)
+  cart = apply_coupons(cart, coupons)
   cart = apply_clearance(cart)
   total = 0 
   cart.each do |item, property|
