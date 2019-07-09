1c21dace31fe0ed5f8276e58516dcd8ffba02434
assane savadogo <assane.savadogo81@gmail.com>
2019-07-09 23:50:07 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,7 +46,7 @@ end
 def checkout(cart, coupons)
   cart = consolidate_cart(cart)
   cart = apply_coupons(cart, coupons)
-  cart = apply_clearance(cart)
+  cart = apply_clearance(cart, coupons)
   total = 0 
   cart.each do |item, property|
     total = property[:price] * property[:count]
