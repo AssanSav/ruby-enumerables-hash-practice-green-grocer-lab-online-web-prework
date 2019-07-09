5d1a267416b26df0e648e6068e109b253051000f
assane savadogo <assane.savadogo81@gmail.com>
2019-07-09 23:45:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -50,7 +50,7 @@ def checkout(cart, coupons)
   total = 0 
   cart.each do |item, property|
     total = property[:price] * property[:count]
-    
+    total >= 100 ? total * 0.1
  binding.pry
   end
 end
